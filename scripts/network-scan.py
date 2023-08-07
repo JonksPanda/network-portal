270a254b7eb9b4ef7cf51aded01af339b14e2ccc
jonkspanda <jionte@gmail.com>
2023-08-07 21:11:51 +0200

    created network scan script

---
+++ scripts/network-scan.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class network:
+    def __init__(self, net_adress, cidr):
+        self.net_adress = net_adress
+        self.cidr = cidr
+        # self.hosts
+        # self.first_usable
+        # self.last_usable
+
+    def calculate_hosts():
+        pass
+
+    def scan_network():
+        pass
+
+
+class host:
+    def __init__(self, ip_adress, name):
+        self.ip_adress = ip_adress
